cfefcdb098556c21d5bf732a5affc974557ee728
Dilip Jagannathan Seshadri <dilip.seshadri@gmail.com>
2020-11-29 08:13:15 -0500

    update to layout

--- layouts.py
+++ layouts.py
@@ -7,6 +7,20 @@ Created on Sat Nov 28 18:39:30 2020
 
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+import os
+from datetime import date
+import json
+
+file_path = os.getcwd()
+df = pd.read_csv(file_path+os.sep+'output_df.csv', index_col=0, parse_dates=True)
+
+years = df.year.unique()
+current_year = date.today().year
+years = [ year for year in years if (year > 2010)  & (year <= current_year) ]
+years = sorted (years)
+genres_list=sorted(df.genres.str.split(',').explode().unique())
+print (genres_list)
 
 tab_style = {
     "borderBottom": "1px solid #d6d6d6",
@@ -18,11 +32,91 @@ tab_style = {
 tab_selected_style = {
     "borderTop": "1px solid #d6d6d6",
     "borderBottom": "1px solid #d6d6d6",
-    "backgroundColor": "white",
-    "color": "blue",
+    "backgroundColor": "black",
+    "color": "white",
     "padding": "10px",
     "font-size": 20,
 }
+
+def get_marks(years):
+    year_marks = {}
+    for year in years:
+        year_dict = {}
+        year_style = {}
+        year_dict['label'] = str(year)
+        year_style['color'] = 'white'
+        year_dict['style'] = year_style
+
+        year_marks[int(year)] = {}
+        year_marks[int(year)] = year_dict
+
+    return year_marks
+
+def get_genres(genres):
+    genre_list = []
+    for genre in genres:
+        genre_dict = {}
+        genre_dict['label'] = genre
+        genre_dict['value'] = genre
+        genre_list.append(genre_dict)
+    print (genre_list)
+    return genre_list
+        
+input = html.Div([
+      		html.H6('Release Year:',
+                              style={
+                                  "display": "inline-block",
+                                  "margin": "0px",
+                                  "color": "white",
+                                  "line-height": "0.1",
+                              },),
+
+      		dcc.RangeSlider(
+              id='year-slider',
+              min=years[0],
+              max=years[-1],
+              step=1,
+              value=[years[0], years[-1]],  
+              marks = get_marks(years),
+      		),
+            html.H6('Genres:',
+                          style={
+                              "display": "inline-block",
+                              "margin": "0px",
+                              "color": "white",
+                              "line-height": "0.1",
+                          },),
+            dcc.Dropdown(
+                id='genres',
+                options=get_genres(genres_list),
+                value=genres_list,
+                multi=True,
+                ),
+            html.H6('Ratings:',
+                          style={
+                              "display": "inline-block",
+                              "margin": "0px",
+                              "color": "white",
+                              "line-height": "0.1",
+                          },),     
+        dcc.RadioItems(
+            id='ratings',
+            options=[
+                {'label': 'IMDB', 'value': 'IMDB'},
+                {'label': 'Rotten Tomatoes', 'value': 'RT'},
+                {'label': 'Metacritic', 'value': 'MC'},
+                {'label': 'Overall', 'value': 'Overall'}
+            ],
+            value='IMDB',
+            style={
+                "display": "inline-block",
+                "margin": "0px",
+                "color": "white",
+                "line-height": "0.1",
+            },
+        labelStyle={'display': 'inline-block'})
+                      
+])
 
 footer = html.Div(
     className="footer",
@@ -97,7 +191,7 @@ layout = html.Div(
                 #"background": "yellow"
             },
         ),
-        html.Div(id="movie-input"),
+        input,
         dcc.Tabs(
             id="all-tabs-inline",
             className="all-tabs-inline",
@@ -153,8 +247,8 @@ layout = html.Div(
                             
                 )
             ],
-            style={"textAlign": "center", "background": "yellow"},
+            style={"textAlign": "center", "background": "orange"},
         ),
     ],
-    style={"background": "#000080"},
+    style={"background": "black"},
 )
